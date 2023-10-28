2e94b0da05586ab55a2fa111ab823ced2a625c11
aliafarinifard <aliafarinifard@outlook.com>
2023-10-28 16:42:51 +0330

    baseUrl updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         domain='dev-1bcnnbqqa6te5mok.us.auth0.com'
         clientId='XIy6JGCKc6U1tqL90TgaquBW5bo9PWFW'
         authorizationParams={{
-            redirect_uri: "http://localhost:5173"
+            redirect_uri: "https://fullstack-real-estate-omega.vercel.app"
         }}
         audience='http://localhost:8000'
         scope='openId profile email'
